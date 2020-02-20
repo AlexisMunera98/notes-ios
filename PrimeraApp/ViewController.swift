a236c63d68473b337c0027c807a7593a3c4d771c
s209e011 <s209e011@b14-209-11.local>
2020-02-20 18:12:36 -0500

    Initial Commit

---
+++ PrimeraApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeraApp
+//
+//  Created by s209e011 on 20/02/20.
+//  Copyright © 2020 udem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
